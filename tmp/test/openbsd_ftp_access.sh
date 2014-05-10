0d471393cb1b0a6fa8e9dff54b0be9497549e9fe
Simon Wunderlin <simon.wunderlin@usb.ch>
2014-05-10 13:47:00 +0200

    cleanup

--- tmp/test/openbsd_ftp_access.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# default location:
-# ftp://ftp.OpenBSD.org/pub/OpenBSD/
-# ftp://mirror.switch.ch/pub/OpenBSD/
-export PKG_PATH=ftp://mirror.switch.ch/pub/OpenBSD/`uname -r`/packages/`machine -a`/
-
-echo $PKG_PATH;
