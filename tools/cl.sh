2ca901977b258671f1da035f08a8e0f2fd7d487a
wus <swunderlin@gmail.com>
2014-05-11 11:18:26 +0200

    added short hash to change log

--- tools/cl.sh
+++ tools/cl.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 d=`dirname $0`
-$d/gitlog-to-changelog > doc/Changelog
+$d/gitlog-to-changelog --format='[%h] %s' > doc/Changelog
 echo -en "\n\n\n" >> doc/Changelog
 cat doc/Changelog.svn >> doc/Changelog
 
