01ae5ebe199b306491ba2dcead69c6b00203fc62
wus <swunderlin@gmail.com>
2014-11-05 15:55:16 +0100

    implemented nodelist example

--- test/nested_struct.c
+++ test/nested_struct.c
@@ -9,7 +9,7 @@ typedef struct {
 typedef struct {
 	char* name;
 	char* value;
-	int count;
+	int length;
 	void** children; // this is actually of type node_u, but it is declared further down
 } nlist;
 
@@ -20,20 +20,62 @@ typedef union {
 
 typedef struct {
 	enum {
-		ROOT,
-		NODE,
-		NODELIST
+		UNDEF,   // 0
+		ROOT,    // 1
+		NODE,    // 2
+		NODELIST // 3
 	} type;
 	node_u* data;
 } node_t;
 
 node_t* init_node(char* name, char* value) {
 	node_t* n = malloc(sizeof(node_t));
+	n->type = NODE;
 	n->data = malloc(sizeof(node_u));
 	n->data->node.name = name;
 	n->data->node.value = value;
 	return n;
 }
+
+node_t* init_nodelist(char* name, char* value) {
+	node_t* n = malloc(sizeof(node_t));
+	n->type = NODELIST;
+	n->data = malloc(sizeof(node_u));
+	n->data->list.name = name;
+	n->data->list.value = value;
+	n->data->list.length = 0;
+	n->data->list.children = NULL;
+	return n;
+}
+
+int node_append(node_t* nodelist, node_t* node) {
+	
+	// check if nodelist accepts children
+	if (nodelist->type != NODELIST)
+		return 1;
+	
+	// allocate memory for children
+	if (nodelist->data->list.length == 0) {
+		// malloc
+		nodelist->data->list.children = malloc(sizeof(node_t));
+		if (nodelist->data->list.children == NULL)
+			return 2;
+	} else {
+		// realloc
+		nodelist->data->list.children = 
+				realloc(nodelist->data->list.children, 
+				sizeof(node_t) * nodelist->data->list.length);
+		if (nodelist->data->list.children == NULL)
+			return 2;
+	}
+	
+	node_t** children = (node_t**) nodelist->data->list.children;
+	children[nodelist->data->list.length] = node;
+	nodelist->data->list.length++;
+	
+	return 0; // success
+}
+
 
 int main(int argc, char* argv[]) {
 	
@@ -42,10 +84,23 @@ int main(int argc, char* argv[]) {
 	n->data->node.name = "name";
 	n->data->node.value = "val";
 	
-	printf("name: %s, value: %s\n", n->data->node.name, n->data->node.value);
+	printf("%d: name: %s, value: %s\n", n->type, n->data->node.name, n->data->node.value);
 	
 	node_t* n1 = init_node("name1", NULL);
-	printf("name: %s, value: %s\n", n1->data->node.name, n1->data->node.value);
+	printf("%d: name: %s, value: %s\n", n1->type, n1->data->node.name, n1->data->node.value);
+	
+	node_t* nl1 = init_nodelist("nl1", "val1");
+	printf("%d: name: %s, value: %s\n", nl1->type, nl1->data->list.name, nl1->data->list.value);
+	
+	node_append(nl1, n1);
+	node_t** children = (node_t**) nl1->data->list.children; 
+	//nl1->data->list.children[0]->data.node->name
+	printf("%d: length: %d, type: %d, name: %s, value: %s\n", nl1->type, 
+			                                 nl1->data->list.length, 
+			                                 children[0]->type,
+			                                 children[0]->data->node.name,
+			                                 children[0]->data->node.value);
+
 	
 	return 0;
 }
